b4d662118ae848f8defdce5d84abf51ecfe3a87c
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-09-04 23:27:31 -0300

    cors error fixed

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 "use strict";
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 
 require("dotenv").config();
 const BcryptGeneratorCheckerRoutes = require("./routes/bcrypt-generator-checker");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", BcryptGeneratorCheckerRoutes.routes);
 
